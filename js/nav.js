f0f2331d8376cb412a775399ea751099f47e3d03
Skyler <skyler2440@gmail.com>
2019-04-16 08:07:16 -0400

    Responsive Desktop Design Work

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,12 @@
 const siteContent = {
     "nav": {
         "nav-item-1": "Home",
-        "nav-item-2": "Resume",
-        "nav-item-3": "Portfolio",
-        "nav-item-4": "Blog",
-        "nav-item-5": "Contact Me",
-        "img-src": "img/logo.png"
-    }};
+        "nav-item-2": "About",
+        "nav-item-3": "Contact",
+        // "nav-item-4": "About the Page",
+        "nav-item-4": "Register",
+        "nav-item-5": "Login"
+        }};
 const navBar = document.querySelectorAll("a");
 for (let i = 0; i <navBar.length; i++) {
     navBar[i].textContent = siteContent["nav"][`nav-item-${i + 1}`];
